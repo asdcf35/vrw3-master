463739fffa2294ab1d77a8c20a8d9304dc59dfc0
asdcf35 <prann1234556@gmail.com>
2024-03-01 16:04:47 -0500

    sdfsdf

--- app/mi/layout.jsx
+++ app/mi/layout.jsx
@@ -1,8 +1,6 @@
 export default function AboutLayout({
 	children,
-}: {
-	children: React.ReactNode;
-}) {
+}){
 	return (
 		<section className="flex flex-col items-center justify-center">
 				{children}
