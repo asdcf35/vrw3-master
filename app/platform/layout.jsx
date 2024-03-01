781da83cb10eee224aaa197b58d9924a63396369
asdcf35 <prann1234556@gmail.com>
2024-03-01 21:02:05 +0000

    Mar 1, 2024, 4:02 PM

--- app/platform/layout.jsx
+++ app/platform/layout.jsx
@@ -1,8 +1,6 @@
 export default function BlogLayout({
 	children,
-}: {
-	children: React.ReactNode;
-}) {
+}){
 	return (
 		<section className="flex flex-col items-center justify-center">
 				{children}
