3115424530c7af3a135a9971ffc469e0801408e5
asdcf35 <prann1234556@gmail.com>
2024-03-02 15:31:35 +0000

    Mar 2, 2024, 10:31 AM

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,6 +1,6 @@
 import { title, subtitle } from "@/components/primitives";
 import { Divider } from "@nextui-org/react";
-
+import Video from "video1.mp4"
 import "./class.css";
 export default function AboutPage() {
   return (
@@ -17,23 +17,29 @@ export default function AboutPage() {
         </div>
       </div>
       <div className="ff"></div>
-      <div className="sp w-[]">
+      <div className="sp flx">
         <p className="df">Backstory</p>
         <div className="limitations-apply"></div>
         <p>
           Kyle is a great fit to be our candidate. He speaks clearly and
-          confidently and gets the point across. <br/><br/>Kyle is from the streets of
-          Sugar Creek, North Carolina. Growing up his family was below the
-          poverty line and unfortunately split and that resulted in him being
-          from a broken home. <br/><br/>He has learned to battle through financial and
-          behavioral hardships. This makes him a fearless leader learning to
-          navigate through a world that may have seemed against him from the
-          start. Kyle is very compassionate throughout his works and he loves
-          helping throughout the community to lead other children who may or may
-          not have been through the same difficulties he has to prove that no
-          matter your background things will work out in the end.
+          confidently and gets the point across. <br />
+          <br />
+          Kyle is from the streets of Sugar Creek, North Carolina. Growing up
+          his family was below the poverty line and unfortunately split and that
+          resulted in him being from a broken home. <br />
+          <br />
+          He has learned to battle through financial and behavioral hardships.
+          This makes him a fearless leader learning to navigate through a world
+          that may have seemed against him from the start. Kyle is very
+          compassionate throughout his works and he loves helping throughout the
+          community to lead other children who may or may not have been through
+          the same difficulties he has to prove that no matter your background
+          things will work out in the end.
         </p>
       </div>
+      <video width="750" height="500" controls>
+        <source src={Video} type="video/mp4" />{" "}
+      </video>
     </>
   );
 }
