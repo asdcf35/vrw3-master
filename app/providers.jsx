f80a85717c0acb8f69d8eb6f04159ee06be62806
asdcf35 <prann1234556@gmail.com>
2024-03-02 15:40:43 +0000

    Mar 2, 2024, 10:40 AM

--- app/providers.jsx
+++ app/providers.jsx
@@ -4,7 +4,14 @@ import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from 'next/navigation'
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+} from "@nextui-org/react";
 
 
 export function Providers({ children, themeProps }) {
