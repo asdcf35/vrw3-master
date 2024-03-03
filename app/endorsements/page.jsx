68c8222aeb67cacb942e224295f08acf61d2e275
asdcf35 <prann1234556@gmail.com>
2024-03-03 02:02:38 +0000

    Mar 2, 2024, 9:02 PM

--- app/endorsements/page.jsx
+++ app/endorsements/page.jsx
@@ -270,63 +270,6 @@ export default function App() {
             )}
           </ModalContent>
         </Modal>
-        <Card
-          isPressable
-          onClick={modalNS.onOpen}
-          isFooterBlurred
-          className="w-full h-[300px] col-span-12 sm:col-span-7"
-        >
-          <CardHeader className="absolute z-10 top-1 flex-col items-start">
-            <p className="text-tiny text-white/60 uppercase font-bold">
-              National Security
-            </p>
-            <h4 className="text-white/90 font-medium text-xl">
-              The US checklist for better security
-            </h4>
-          </CardHeader>
-          <Image
-            removeWrapper
-            alt="Relaxing app background"
-            className="z-0 w-full h-full object-cover"
-            src="https://api.army.mil/e2/c/images/2023/11/03/b4b385f7/size1.jpg"
-          />
-          <CardFooter className="absolute bg-black/40 bottom-0 z-10 border-t-1 border-default-600 dark:border-default-100">
-            <div className="flex flex-grow gap-2 items-center">
-              <div className="flex flex-col marigold">
-                <p className="text-tiny text-white/60">
-                  What we think the government should do first for national
-                  security.
-                </p>
-              </div>
-            </div>
-          </CardFooter>
-        </Card>
-        <Modal isOpen={modalNS.isOpen} onOpenChange={modalNS.onOpenChange}>
-          <ModalContent>
-            {(onClose) => (
-              <>
-                <ModalHeader className="flex flex-col gap-1">
-                  The US checklist for better security
-                </ModalHeader>
-                <ModalBody>
-                  <p>
-                    We should back out of the Middle East and encourage other
-                    countries to step forward. The U.S. is the highest-paying
-                    country for wars in the Middle East. An example was the
-                    Israel-Palestine War. We spent way more money than other
-                    countries and we could have saved way more by encouraging
-                    others to put forward their fair share.
-                  </p>
-                </ModalBody>
-                <ModalFooter>
-                  <Button color="primary" onPress={onClose}>
-                    Close
-                  </Button>
-                </ModalFooter>
-              </>
-            )}
-          </ModalContent>
-        </Modal>
       </div>
     </>
   );
